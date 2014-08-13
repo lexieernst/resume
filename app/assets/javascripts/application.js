aa18c4418f6183c3caf3784b5605017bfce93d12
lexieernst <ldernst@umich.edu>
2014-08-13 01:12:27 -0400

    hover with white font black background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,19 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//=require jquery
+//=require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$( document ).ready(function() {	
+  $('h1').mouseenter(function() {
+  	$(this).css("font-weight", "normal")
+    	.animate({'letter-spacing': '7px'}, 1000);
+  });
+  $('h1').mouseleave(function() {
+  	$(this).css("font-weight", "bold")
+  		.animate({'letter-spacing': '0px'}, 1000);
+  });
+});
--- app/assets/javascripts/welcome.js
+++
@@ -1,9 +0,0 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
-
-$( document ).ready(function() {
-$(".nav").on("mouseenter", function(event) {
-    $(this).addClass("slow", function() {
-    });
-});
